efa2a09ccaa171856aed36e9bb64af38dbd78b10
Mohamed El Amine Boudjoghra <95423980+aminebdj@users.noreply.github.com>
2024-06-17 15:33:09 +0400

    Updated download link

--- scripts/get_class_agn_masks.sh
+++ scripts/get_class_agn_masks.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+if [ -d "./OpenYOLO3D" ]
+then
+    echo "OpenYOLO3D already exists!"
+else
+    echo "Downloading OpenYOLO3D.zip ..."
+    gdown 1FneLaYaClWDO51L9lIvlTQbheh5SfOFD
+    echo "Unzipping OpenYOLO3D.zip to OpenYOLO3D..."
+    unzip OpenYOLO3D.zip
+fi
 echo "Moving OpenYOLO3D/output to ./ ..."
 mv ./OpenYOLO3D/output ./
 
--- scripts/get_replica_dataset.sh
+++ scripts/get_replica_dataset.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+if [ -d "./OpenYOLO3D" ]
+then
+    echo "OpenYOLO3D already exists!"
+else
+    echo "Downloading OpenYOLO3D.zip ..."
+    gdown 1FneLaYaClWDO51L9lIvlTQbheh5SfOFD
+    echo "Unzipping OpenYOLO3D.zip to OpenYOLO3D..."
+    unzip OpenYOLO3D.zip
+fi
 mkdir data/
 echo "Moving OpenYOLO3D/replica to data/replica ..."
 mv ./OpenYOLO3D/replica ./data/
